8541ab62f79d158ef1106213f7196da4140bd6ef
Ismael Muñoz <Prosilver05@gmail.com>
2021-08-09 20:55:41 -0500

    should remove .next folder and added profile testing page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84ee6da12f23b5037193"; }
+/******/ 		__webpack_require__.h = function() { return "62c48cca19c38b4f936e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/profile.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Link from "next/link";
+
+const profile = () => {
+  return (
+    <div id="profilePage">
+      <Link href="/">
+        <h1>Profile Tab</h1>
+      </Link>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia est,
+        ipsam error excepturi libero laborum eligendi sit eos. Aliquam sit
+        voluptatibus consectetur ipsam officiis adipisci, consequuntur
+        voluptatum magnam nemo eos.
+      </p>
+    </div>
+  );
+};
+
+export default profile;
